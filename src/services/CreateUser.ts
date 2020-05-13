1ce5dcd8651924cb1d508162aa471dbf63ccdc83
haironCardoso <hairon.cardoso@gmail.com>
2020-05-13 15:09:11 -0300

    conceitos typescript

--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -5,13 +5,13 @@ interface TechsData{
 }
 
 interface CreateUserData{
-    name: string
+    name?: string
     email: string
     phone: string,
     techs: Array<string | TechsData>
 }
 
-export default function createUser({name, email, phone,techs}: CreateUserData){
+export default function createUser({name ='', email, phone,techs}: CreateUserData){
     
     const user = {
         name,
